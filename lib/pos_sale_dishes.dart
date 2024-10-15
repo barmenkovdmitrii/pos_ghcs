ab7362eeab223b121648e129423bf9e4a04860e2
barmenkovdmitrii <bds@porter.pro>
2024-10-15 18:01:18 +0300

    Форма для чека

--- lib/pos_sale_dishes.dart
+++ lib/pos_sale_dishes.dart
@@ -48,54 +48,174 @@ class _MyFormState extends State<MyForm> {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Название точки'),
-                  Text('Имя кассира'),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Center(
-                child: Text(
-                  _currentTime,
-                  style: TextStyle(fontSize: 24),
+        // Первая строка (15% высоты экрана)
+        Container(
+          height: screenHeight * 0.15,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Название точки'),
+                    Text('Имя кассира'),
+                  ],
                 ),
               ),
-            ),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // Действие для первой кнопки
-                    },
-                    child: Text('Кнопка 1'),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    _currentTime,
+                    style: TextStyle(fontSize: 24),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Действие для второй кнопки
-                    },
-                    child: Text('Кнопка 2'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Действие для третьей кнопки
-                    },
-                    child: Text('Кнопка 3'),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ],
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    // Кнопки в первой строке
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Действие для первой кнопки
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          ),
+                        ),
+                        child: Text('Кнопка 1'),
+                      ),
+                    ),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Действие для второй кнопки
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          ),
+                        ),
+                        child: Text('Кнопка 2'),
+                      ),
+                    ),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Действие для третьей кнопки
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          ),
+                        ),
+                        child: Text('Кнопка 3'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        // Вторая строка (70% высоты экрана)
+        Container(
+          height: screenHeight * 0.70,
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.grey[300],
+                  child: Center(
+                    child: Text('Первый столбец'),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 6,
+                      childAspectRatio: 1.0,
+                    ),
+                    itemCount: 36,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: EdgeInsets.all(4.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Действие для кнопки
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero,
+                            ),
+                          ),
+                          child: Text('Кнопка ${index + 1}'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        // Третья строка (15% высоты экрана)
+        Container(
+          height: screenHeight * 0.15,
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Действие для первой кнопки в третьем столбце
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                        ),
+                      ),
+                      child: Text('Кнопка A'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Действие для второй кнопки в третьем столбце
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                        ),
+                      ),
+                      child: Text('Кнопка B'),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.grey[200], // Цвет фона для второго столбца
+                  child: Center(
+                    child: Text('Второй столбец'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
