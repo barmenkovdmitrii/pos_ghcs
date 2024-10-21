2a4e518ab3babd526d201e49e4eec7cb5c0c7bc8
barmenkovdmitrii <bds@porter.pro>
2024-10-21 11:26:26 +0300

    Добавлено стрелки право лево для вкладо

--- lib/pos_sale_dishes.dart
+++ lib/pos_sale_dishes.dart
@@ -1,61 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   List<List<Map<String, dynamic>>> records = [
-    [] // Инициализируем только одну вкладку
-  ]; // Список для хранения записей для каждой вкладки
+    []
+  ]; // Инициализируем только одну вкладку
   late TabController _tabController; // Контроллер для вкладок
   late TabController _buttonTabController; // Контроллер для вкладок кнопок
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(
-        length: records.length,
-        vsync: this); // Инициализация контроллера с одной вкладкой
-    _buttonTabController = TabController(
-        length: 5, vsync: this); // Изменяем количество вкладок кнопок на 5
+    _tabController = TabController(length: records.length, vsync: this);
+    _buttonTabController = TabController(length: 5, vsync: this);
   }
 
   @override
   void dispose() {
-    _tabController.dispose(); // Освобождение ресурсов контроллера
-    _buttonTabController.dispose(); // Освобождение ресурсов контроллера кнопок
+    _tabController.dispose();
+    _buttonTabController.dispose();
     super.dispose();
   }
 
   void _addRecord(String title, String price) {
     setState(() {
-      // Получаем индекс текущей вкладки
       int currentIndex = _tabController.index;
 
-      // Проверяем, нужно ли добавить новую вкладку
       if (records[currentIndex].length >= 4) {
-        // Если в текущей вкладке 4 записи, создаем новую вкладку
-        records.add([]); // Добавляем новый список для новой вкладки
+        records.add([]);
         _tabController = TabController(length: records.length, vsync: this);
       }
 
-      // Устанавливаем фокус на последнюю вкладку
       _tabController.index = _tabController.length - 1;
-
-      // Теперь добавляем запись в новую или текущую вкладку
-      int newIndex = _tabController.index; // Получаем индекс новой вкладки
+      int newIndex = _tabController.index;
       records[newIndex].add({
         'title': title,
         'price': price,
-        'count': 1, // Начальное количество нажатий
+        'count': 1,
       });
     });
   }
@@ -63,8 +52,6 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   void _removeRecord(int index, int recordIndex) {
     setState(() {
       records[index].removeAt(recordIndex);
-
-      // Проверяем, если вкладка пустая, удаляем её
       if (records[index].isEmpty) {
         records.removeAt(index);
         if (_tabController.length > 1) {
@@ -91,41 +78,32 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
             double firstRowHeight = screenHeight * 0.10;
             double secondRowHeight = screenHeight * 0.90;
 
-            double buttonHeight = (screenHeight * 0.71) / 6;
-            double buttonWidth = screenWidth * 0.63 / 6;
-
             return Column(
               children: [
                 // Первая строка
                 Row(
                   children: [
                     Expanded(
-                      child: Container(
-                        height: firstRowHeight,
-                        color: Colors.red,
-                        child: const Center(
-                            child: Text('1/3',
-                                style: TextStyle(color: Colors.white))),
-                      ),
-                    ),
+                        child: Container(
+                            height: firstRowHeight,
+                            color: Colors.red,
+                            child: Center(
+                                child: Text('1/3',
+                                    style: TextStyle(color: Colors.white))))),
                     Expanded(
-                      child: Container(
-                        height: firstRowHeight,
-                        color: Colors.green,
-                        child: const Center(
-                            child: Text('1/3',
-                                style: TextStyle(color: Colors.white))),
-                      ),
-                    ),
+                        child: Container(
+                            height: firstRowHeight,
+                            color: Colors.green,
+                            child: Center(
+                                child: Text('1/3',
+                                    style: TextStyle(color: Colors.white))))),
                     Expanded(
-                      child: Container(
-                        height: firstRowHeight,
-                        color: Colors.blue,
-                        child: const Center(
-                            child: Text('1/3',
-                                style: TextStyle(color: Colors.white))),
-                      ),
-                    ),
+                        child: Container(
+                            height: firstRowHeight,
+                            color: Colors.blue,
+                            child: Center(
+                                child: Text('1/3',
+                                    style: TextStyle(color: Colors.white))))),
                   ],
                 ),
                 // Вторая строка с вкладками
@@ -160,7 +138,7 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
                                         subtitle: Text(
                                             'Цена: ${records[index][recordIndex]['price']}, Количество: ${records[index][recordIndex]['count']}'),
                                         trailing: IconButton(
-                                          icon: const Icon(Icons.delete,
+                                          icon: Icon(Icons.delete,
                                               color: Colors.red),
                                           onPressed: () {
                                             _removeRecord(index, recordIndex);
@@ -181,49 +159,11 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
                       child: Container(
                         height: secondRowHeight,
                         color: Colors.purple,
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: TabBarView(
-                                controller: _buttonTabController,
-                                children: List.generate(5, (tabIndex) {
-                                  return GridView.builder(
-                                    physics: const NeverScrollableScrollPhysics(),
-                                    gridDelegate:
-                                        SliverGridDelegateWithFixedCrossAxisCount(
-                                      crossAxisCount:
-                                          6, // Количество кнопок в строке
-                                      childAspectRatio: buttonWidth /
-                                          buttonHeight, // Соотношение сторон
-                                    ),
-                                    itemCount: 36,
-                                    itemBuilder: (context, index) {
-                                      int buttonIndex = index +
-                                          (tabIndex * 36) +
-                                          1; // Индекс кнопки
-                                      return CustomButton(
-                                        title: 'Кнопка $buttonIndex',
-                                        price: '${buttonIndex * 3}.00',
-                                        weight:
-                                            '${buttonIndex * 0.5} кг', // Добавляем вес
-                                        onPressed: () {
-                                          // Добавляем запись при нажатии на кнопку
-                                          _addRecord('Кнопка $buttonIndex',
-                                              '${buttonIndex * 3}.00');
-                                        },
-                                      );
-                                    },
-                                  );
-                                }),
-                              ),
-                            ),
-                            TabBar(
-                              controller: _buttonTabController,
-                              tabs: List.generate(5, (index) {
-                                return Tab(text: 'Кнопки ${index + 1}');
-                              }),
-                            ),
-                          ],
+                        child: TabButtonPanel(
+                          buttonTabController: _buttonTabController,
+                          onButtonPressed: (title, price) {
+                            _addRecord(title, price);
+                          },
                         ),
                       ),
                     ),
@@ -238,86 +178,171 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   }
 }
 
+class TabButtonPanel extends StatelessWidget {
+  final TabController buttonTabController;
+  final Function(String title, String price) onButtonPressed;
+
+  TabButtonPanel(
+      {required this.buttonTabController, required this.onButtonPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    double buttonHeight = (screenHeight * 0.80) / 6; // Высота кнопки
+    double buttonWidth = (screenWidth * 0.64) / 6; // Ширина кнопки
+
+    return Column(
+      children: [
+        Expanded(
+          child: TabBarView(
+            controller: buttonTabController,
+            children: List.generate(5, (tabIndex) {
+              return GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 6, // Количество кнопок в строке
+                  childAspectRatio:
+                      buttonWidth / buttonHeight, // Соотношение сторон
+                ),
+                itemCount: 36,
+                itemBuilder: (context, index) {
+                  int buttonIndex =
+                      index + (tabIndex * 36) + 1; // Индекс кнопки
+                  return CustomButton(
+                    title: 'Кнопка $buttonIndex',
+                    price: '${buttonIndex * 3}.00',
+                    weight: '${buttonIndex * 0.5} кг', // Добавляем вес
+                    onPressed: () {
+                      // Добавляем запись при нажатии на кнопку
+                      onButtonPressed(
+                          'Кнопка $buttonIndex', '${buttonIndex * 3}.00');
+                    },
+                    buttonHeight: buttonHeight, // Передаем высоту кнопки
+                    buttonWidth: buttonWidth, // Передаем ширину кнопки
+                  );
+                },
+              );
+            }),
+          ),
+        ),
+        Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                if (buttonTabController.index > 0) {
+                  buttonTabController.animateTo(buttonTabController.index - 1);
+                }
+              },
+            ),
+            Expanded(
+              child: TabBar(
+                controller: buttonTabController,
+                isScrollable: true, // Позволяет прокручивать вкладки
+                tabs: List.generate(5, (index) {
+                  return Tab(
+                    child: Text(
+                      'Кнопки ${index + 1}',
+                      style: TextStyle(
+                          color:
+                              Colors.white), // Устанавливаем белый цвет текста
+                    ),
+                  );
+                }),
+                indicatorColor: Colors.white, // Устанавливаем цвет индикатора
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_forward),
+              onPressed: () {
+                if (buttonTabController.index <
+                    buttonTabController.length - 1) {
+                  buttonTabController.animateTo(buttonTabController.index + 1);
+                }
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
 class CustomButton extends StatelessWidget {
   final String title;
   final String price;
   final String weight; // Добавляем параметр для веса
   final VoidCallback onPressed; // Добавляем колбек для нажатия
+  final double buttonHeight; // Высота кнопки
+  final double buttonWidth; // Ширина кнопки
 
-  const CustomButton({super.key, 
+  CustomButton({
     required this.title,
     required this.price,
     required this.weight, // Инициализируем параметр веса
     required this.onPressed, // Инициализируем колбек
+    required this.buttonHeight, // Инициализируем высоту кнопки
+    required this.buttonWidth, // Инициализируем ширину кнопки
   });
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        // Получаем размеры кнопки
-        double buttonHeight = constraints.maxHeight; // Высота кнопки
-
-        // Вычисляем размер шрифта в зависимости от высоты кнопки
-        double titleFontSize = buttonHeight * 0.15; // 15% от высоты кнопки
-        double priceFontSize = buttonHeight * 0.1; // 10% от высоты кнопки
-        double weightFontSize = buttonHeight * 0.1; // 10% от высоты кнопки
-
-        return GestureDetector(
-          onTap: onPressed, // Вызываем колбек при нажатии
-          child: Container(
-            margin: const EdgeInsets.all(4.0), // Отступы между кнопками
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: onPressed, // Вызываем колбек при нажатии
+      child: Container(
+        height: buttonHeight, // Устанавливаем высоту кнопки
+        width: buttonWidth, // Устанавливаем ширину кнопки
+        margin: EdgeInsets.all(4.0), // Отступы между кнопками
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 10,
+              left: 10,
+              child: Container(
+                width: buttonWidth - 20, // Уменьшаем ширину для отступов
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: buttonHeight * 0.15, // 15% от высоты кнопки
+                    fontWeight: FontWeight.bold,
+                  ),
+                  maxLines: 2, // Максимальное количество строк
+                  overflow: TextOverflow.visible, // Перенос текста
+                ),
+              ),
             ),
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 10,
-                  left: 10,
-                  child: SizedBox(
-                    width: constraints.maxWidth -
-                        20, // Уменьшаем ширину для отступов
-                    child: Text(
-                      title,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: titleFontSize,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      maxLines: 2, // Максимальное количество строк
-                      overflow: TextOverflow.visible, // Перенос текста
-                    ),
-                  ),
+            Positioned(
+              bottom: 10,
+              left: 10,
+              child: Text(
+                weight, // Отображаем вес
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: buttonHeight * 0.1, // 10% от высоты кнопки
                 ),
-                Positioned(
-                  bottom: 10,
-                  left: 10,
-                  child: Text(
-                    weight, // Отображаем вес
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: weightFontSize,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 10,
-                  right: 10,
-                  child: Text(
-                    price,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: priceFontSize,
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        );
-      },
+            Positioned(
+              bottom: 10,
+              right: 10,
+              child: Text(
+                price,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: buttonHeight * 0.1, // 10% от высоты кнопки
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
