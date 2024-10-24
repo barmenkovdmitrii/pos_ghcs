92d6f2ee2901a36ba303d027313a3f318c181628
barmenkovdmitrii <bds@porter.pro>
2024-10-24 12:10:57 +0300

    Распределение колонок и столбцов

--- lib/pos_sale_dishes.dart
+++ lib/pos_sale_dishes.dart
@@ -5,8 +5,6 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -66,7 +64,8 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   void _removeRecord(int index, int recordIndex) {
     setState(() {
       int buttonIndex = records[index][recordIndex]['buttonIndex'];
-      buttonClickCounts[buttonIndex] = (buttonClickCounts[buttonIndex] ?? 1) - 1;
+      buttonClickCounts[buttonIndex] =
+          (buttonClickCounts[buttonIndex] ?? 1) - 1;
       records[index][recordIndex]['count'] -= 1;
 
       if (records[index][recordIndex]['count'] <= 0) {
@@ -76,9 +75,11 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
       if (records[index].isEmpty) {
         records.removeAt(index);
         if (_tabController.length > 1) {
-          _tabController = TabController(length: _tabController.length - 1, vsync: this);
+          _tabController =
+              TabController(length: _tabController.length - 1, vsync: this);
           if (_tabController.index >= index) {
-            _tabController.index = _tabController.index > 0 ? _tabController.index - 1 : 0;
+            _tabController.index =
+                _tabController.index > 0 ? _tabController.index - 1 : 0;
           }
         }
       }
@@ -87,8 +88,22 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
 
   void _incrementButtonClick(int buttonIndex) {
     setState(() {
-      buttonClickCounts[buttonIndex] = (buttonClickCounts[buttonIndex] ?? 0) + 1;
+      buttonClickCounts[buttonIndex] =
+          (buttonClickCounts[buttonIndex] ?? 0) + 1;
     });
+  }
+
+  // Метод для вычисления общей суммы
+  double _calculateTotal() {
+    double total = 0.0;
+    for (var tabRecords in records) {
+      for (var record in tabRecords) {
+        double price = double.tryParse(record['price']) ?? 0.0;
+        int count = record['count'];
+        total += price * count;
+      }
+    }
+    return total;
   }
 
   @override
@@ -100,16 +115,36 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
             double screenHeight = MediaQuery.of(context).size.height;
             double screenWidth = MediaQuery.of(context).size.width;
 
-            double firstRowHeight = screenHeight * 0.10;
-            double secondRowHeight = screenHeight * 0.90;
+            double firstRowHeight =
+                screenHeight * 0.10; // 10% для последней строки
+            double secondRowHeight =
+                screenHeight * 0.90; // 90% для основной части
 
             return Column(
               children: [
                 Row(
                   children: [
-                    Expanded(child: Container(height: firstRowHeight, color: Colors.red, child: const Center(child: Text('1/3', style: TextStyle(color: Colors.white))))),
-                    Expanded(child: Container(height: firstRowHeight, color: Colors.green, child: const Center(child: Text('1/3', style: TextStyle(color: Colors.white))))),
-                    Expanded(child: Container(height: firstRowHeight, color: Colors.blue, child: const Center(child: Text('1/3', style: TextStyle(color: Colors.white))))),
+                    Expanded(
+                        child: Container(
+                            height: firstRowHeight,
+                            color: Colors.red,
+                            child: Center(
+                                child: Text('1/3',
+                                    style: TextStyle(color: Colors.white))))),
+                    Expanded(
+                        child: Container(
+                            height: firstRowHeight,
+                            color: Colors.green,
+                            child: Center(
+                                child: Text('1/3',
+                                    style: TextStyle(color: Colors.white))))),
+                    Expanded(
+                        child: Container(
+                            height: firstRowHeight,
+                            color: Colors.blue,
+                            child: Center(
+                                child: Text('1/3',
+                                    style: TextStyle(color: Colors.white))))),
                   ],
                 ),
                 Row(
@@ -124,7 +159,8 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
                             TabBar(
                               controller: _tabController,
                               isScrollable: true,
-                              tabs: List.generate(_tabController.length, (index) {
+                              tabs:
+                                  List.generate(_tabController.length, (index) {
                                 return Tab(text: 'Вкладка ${index + 1}');
                               }),
                             ),
@@ -132,7 +168,31 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
                               child: CustomTabBarView(
                                 tabController: _tabController,
                                 records: records,
-                                onRemoveRecord: _removeRecord
+                                onRemoveRecord: _removeRecord,
+                              ),
+                            ),
+                            // Добавляем строку для отображения общей суммы
+                            Container(
+                              height: firstRowHeight, // 10% от высоты экрана
+                              color: Colors.yellow,
+                              child: Center(
+                                child: Text(
+                                  'Итого: ${_calculateTotal().toStringAsFixed(2)}',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 20),
+                                ),
+                              ),
+                            ),
+                            // Добавляем новую строку
+                            Container(
+                              height: firstRowHeight, // 10% от высоты экрана
+                              color: Colors.lightBlue,
+                              child: Center(
+                                child: Text(
+                                  'Дополнительная строка',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 20),
+                                ),
                               ),
                             ),
                           ],
@@ -172,7 +232,7 @@ class CustomTabBarView extends StatelessWidget {
   final List<List<Map<String, dynamic>>> records;
   final Function(int index, int recordIndex) onRemoveRecord;
 
-  const CustomTabBarView({super.key, 
+  CustomTabBarView({
     required this.tabController,
     required this.records,
     required this.onRemoveRecord,
@@ -198,7 +258,7 @@ class CustomListView extends StatelessWidget {
   final List<Map<String, dynamic>> records;
   final Function(int recordIndex) onRemoveRecord;
 
-  const CustomListView({super.key, 
+  CustomListView({
     required this.records,
     required this.onRemoveRecord,
   });
@@ -208,11 +268,17 @@ class CustomListView extends StatelessWidget {
     return ListView.builder(
       itemCount: records.length,
       itemBuilder: (context, recordIndex) {
+        // Преобразуем цену в double
+        double price = double.tryParse(records[recordIndex]['price']) ?? 0.0;
+        int count = records[recordIndex]['count'];
+        double total = price * count; // Вычисляем итог
+
         return ListTile(
           title: Text('Кнопка: ${records[recordIndex]['title']}'),
-          subtitle: Text('Цена: ${records[recordIndex]['price']}, Количество: ${records[recordIndex]['count']}'),
+          subtitle: Text(
+              'Цена: ${records[recordIndex]['price']}, Количество: $count, Итог: ${total.toStringAsFixed(2)}'),
           trailing: IconButton(
-            icon: const Icon(Icons.delete, color: Colors.red),
+            icon: Icon(Icons.delete, color: Colors.red),
             onPressed: () {
               onRemoveRecord(recordIndex);
             },
@@ -229,7 +295,7 @@ class TabButtonPanel extends StatelessWidget {
   final Function(int buttonIndex) onButtonClick;
   final Map<int, int> buttonClickCounts;
 
-  const TabButtonPanel({super.key, 
+  TabButtonPanel({
     required this.buttonTabController,
     required this.onButtonPressed,
     required this.onButtonClick,
@@ -251,7 +317,7 @@ class TabButtonPanel extends StatelessWidget {
             controller: buttonTabController,
             children: List.generate(15, (tabIndex) {
               return GridView.builder(
-                physics: const NeverScrollableScrollPhysics(),
+                physics: NeverScrollableScrollPhysics(),
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 6,
                   childAspectRatio: buttonWidth / buttonHeight,
@@ -264,7 +330,8 @@ class TabButtonPanel extends StatelessWidget {
                     price: '${buttonIndex * 3}.00',
                     weight: '${buttonIndex * 0.5} кг',
                     onPressed: () {
-                      onButtonPressed('Кнопка $buttonIndex', '${buttonIndex * 3}.00', buttonIndex);
+                      onButtonPressed('Кнопка $buttonIndex',
+                          '${buttonIndex * 3}.00', buttonIndex);
                       onButtonClick(buttonIndex);
                     },
                     buttonHeight: buttonHeight,
@@ -279,10 +346,10 @@ class TabButtonPanel extends StatelessWidget {
         Row(
           children: [
             IconButton(
-              icon: const Icon(Icons.arrow_back),
+              icon: Icon(Icons.arrow_back),
               onPressed: () {
                 if (buttonTabController.index > 0) {
-                  buttonTabController.animateTo(buttonTabController.index - 4);
+                  buttonTabController.animateTo(buttonTabController.index - 1);
                 }
               },
             ),
@@ -292,11 +359,11 @@ class TabButtonPanel extends StatelessWidget {
                 isScrollable: true,
                 tabs: List.generate(15, (index) {
                   return Tab(
-                    child: SizedBox(
-                                           width: MediaQuery.of(context).size.width * 0.66 / 4,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.66 / 4,
                       child: Text(
                         'Кнопки с длинной надписью что бы было ${index + 1}',
-                        style: const TextStyle(color: Colors.white),
+                        style: TextStyle(color: Colors.white),
                         maxLines: 2,
                         overflow: TextOverflow.ellipsis,
                       ),
@@ -307,10 +374,11 @@ class TabButtonPanel extends StatelessWidget {
               ),
             ),
             IconButton(
-              icon: const Icon(Icons.arrow_forward),
+              icon: Icon(Icons.arrow_forward),
               onPressed: () {
-                if (buttonTabController.index < buttonTabController.length - 1) {
-                  buttonTabController.animateTo(buttonTabController.index + 4);
+                if (buttonTabController.index <
+                    buttonTabController.length - 1) {
+                  buttonTabController.animateTo(buttonTabController.index + 1);
                 }
               },
             ),
@@ -330,7 +398,7 @@ class CustomButton extends StatelessWidget {
   final double buttonWidth;
   final int clickCount;
 
-  const CustomButton({super.key, 
+  CustomButton({
     required this.title,
     required this.price,
     required this.weight,
@@ -347,7 +415,7 @@ class CustomButton extends StatelessWidget {
       child: Container(
         height: buttonHeight,
         width: buttonWidth,
-        margin: const EdgeInsets.all(4.0),
+        margin: EdgeInsets.all(4.0),
         decoration: BoxDecoration(
           color: Colors.blue,
           borderRadius: BorderRadius.circular(10),
@@ -357,7 +425,7 @@ class CustomButton extends StatelessWidget {
             Positioned(
               top: 10,
               left: 10,
-              child: SizedBox(
+              child: Container(
                 width: buttonWidth - 20,
                 child: Text(
                   title,
@@ -398,7 +466,7 @@ class CustomButton extends StatelessWidget {
                 top: 10,
                 right: 10,
                 child: Container(
-                  padding: const EdgeInsets.all(4.0),
+                  padding: EdgeInsets.all(4.0),
                   decoration: BoxDecoration(
                     color: Colors.red,
                     borderRadius: BorderRadius.circular(10),
